4e408d8eca872c4405f5cafb8d135a88fa9faddb
Dean Summer <deanlovesmargie@gmail.com>
2020-08-24 05:15:53 -0500

    add description

--- commands/help.js
+++ commands/help.js
@@ -28,7 +28,7 @@ module.exports = {
         data.push(`**Name:** ${command.name}`);
 
         if (command.aliases) data.push(`**Aliases:** ${command.aliases.join(', ')}`);
-        if (command.description) data.push(`**Description:**`);
+        if (command.description) data.push(`**Description:** ${command.description}`);
         if (command.usage) data.push(`**Usage:** ${prefix}${command.name} ${command.usage}`);
 
         data.push(`**Cooldown:** ${command.cooldown || 3} second(s)`);
